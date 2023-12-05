99217e283937be4e7a67a93aa3e1f1b488934a96
loiaconof <francescoloiacono95@outlook.it>
2023-12-05 18:01:31 +0100

    refactor: switch from vue to nuxt

---
+++ nuxt/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    iugug
+    <NuxtWelcome />
+  </div>
+</template>
---
+++ nuxt/nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  devServer: {
+    // host: '127.0.0.1', // default: localhost
+    port: 8001
+  },
+})
