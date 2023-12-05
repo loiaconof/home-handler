1648c69bee6f6674d577398dda4853f716413467
loiaconof <francescoloiacono95@outlook.it>
2023-12-05 18:53:45 +0100

    update: hmr on nuxt

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -5,4 +5,12 @@ export default defineNuxtConfig({
     // host: '127.0.0.1', // default: localhost
     port: 8001
   },
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+        interval: 1000,
+      }
+    }
+  }
 })
