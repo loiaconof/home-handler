cf8d411d53c3c147d3209454d831ff75a17da29e
loiaconof <francescoloiacono95@outlook.it>
2023-12-15 13:03:46 +0100

    refactor: downgrade from symfony 7.0 to symfony 6.4

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -2,9 +2,4 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
