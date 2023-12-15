adec7008659a39e1ab88a12174f6b11c695b3254
loiaconof <francescoloiacono95@outlook.it>
2023-12-15 13:08:18 +0100

    update: composer require symfony/security-bundle

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     SymfonyBundles\JsonRequestBundle\JsonRequestBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
